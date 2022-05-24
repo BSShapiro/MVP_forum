5be509d47e54ed8d0e343e78f1435f6cacc2c4db
bsshapiro <bsshapiro7@gmail.com>
2022-05-24 15:50:59 -0500

    Second commit

--- public/main.js
+++ public/main.js
@@ -1,11 +1,85 @@
+let current;
+
 function getProfile(){
-    fetch('http://localhost:3000/api/profiles')
-    .then(response => response.json())
-    .then(handleData)
+    // fetch('http://localhost:3000/api/profiles')
+    // .then(response => response.json())
+    // .then(handleData)
+    let nav = document.getElementById('navcontainer');
+    nav.style.display = 'none';
+    let makepost = document.getElementById('makepost');
+    makepost.style.display = 'none';
+    let loginbtn = document.getElementById('loginbtn');
+    let createAccount = document.getElementById('createAccount');
+    let username = document.getElementById('username');
+    let password = document.getElementById('password');
+
+    loginbtn.addEventListener('click', () =>{
+        const data = {
+            user_name: username.value,
+            password: password.value
+         };
+         if(data.user_name.length === 0 || data.password.length === 0){
+             window.alert('Please enter your username and password or create an account.');
+             return undefined;
+         }
+         console.log(data);
+         fetch('http://localhost:3000/api/login', {
+          method: 'POST', // or 'PUT'
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(handleData)
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    })
+
+    createAccount.addEventListener('click', () => {
+        const data = {
+            user_name: username.value,
+            password: password.value
+         };
+         if(data.user_name.length === 0 || data.password.length === 0){
+            window.alert('Please enter your username and password or create an account.');
+            return undefined;
+        }
+         console.log(data);
+         fetch('http://localhost:3000/api/create', {
+          method: 'POST', // or 'PUT'
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(handleData)
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    })
 }
 
 function handleData(data){
     console.log(data);
+    if(data === 'Account does not exist, please make a new account.'){
+        window.alert('Account does not exist, please make a new account.')
+    } else if(data === 'Password is incorrect.'){
+        window.alert('Password is incorrect')
+    } else if(data === 'An error has occured.'){
+        window.alert('An error has occured.')
+    } else if(data === 'Username already exists, please enter a new username.'){
+        window.alert('Username already exists, please enter a new username.')
+    } else{
+        let nav = document.getElementById('navcontainer');
+        nav.style.display = 'block';
+        current = data.id;
+
+        let login = document.getElementById('login');
+        login.style.display = 'none';
+    }
 }
 
 getProfile();
--- server.js
+++ server.js
@@ -15,22 +15,56 @@ app.get("/", async (req, res) => {
     
 })
 
-app.get("/api/profiles", async (req, res) => {
+app.post("/api/login", async (req, res) => {
     let worked;
     let data;
     try{
-        data = await db.query("SELECT * FROM profile;")
+        data = await db.query(`SELECT * FROM profile WHERE user_name = '${req.body.user_name}';`);
         worked = true;
+        console.log(data.rows);
     }catch(err){
         console.error(err);
         worked = false;
     }
-    if(worked === true){
-        res.send(data.rows);
+    if(data.rows.length === 0){
+        res.json('Account does not exist, please make a new account.');
+    } else if(data.rows[0].password !== req.body.password){
+        res.json('Password is incorrect.');
+    } else if(data.rows[0].password === req.body.password && worked === true){
+        res.json(data.rows[0]);
     } else if(worked === false){
-        res.status(500).send('An error has occurred.')
+        res.status(500).json('An error has occured.');
     }
 });
+
+app.post("/api/create", async (req, res) => {
+    let worked;
+    let data;
+    try{
+        data = await db.query(`SELECT * FROM profile WHERE user_name = '${req.body.user_name}';`)
+    } catch(err){
+        console.error(err);
+    }
+    if(data.rows.length === 0){
+        try{
+            await db.query(`INSERT INTO profile(user_name, password) VALUES ('${req.body.user_name}', '${req.body.password}');`);
+            data = await db.query(`SELECT * FROM profile WHERE user_name = '${req.body.user_name}'`);
+            worked = true;
+        } catch(err){
+            console.error(err);
+            worked = false;
+        }
+        if(worked === true){
+            res.json(data.rows[0])
+        } else if(worked === false){
+            res.status(500).json('An error has occured.')
+        }
+    } else if(data.rows.length > 0){
+        res.json('Username already exists, please enter a new username.')
+    }
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`listening on Port ${PORT}`);
