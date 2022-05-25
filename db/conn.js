9d2129e70a8c472e9b3016041188087fa2bb7b65
bsshapiro <bsshapiro7@gmail.com>
2022-05-25 13:08:21 -0500

    Server

--- db/conn.js
+++ db/conn.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 const pool = new Pool({
   // Format: postgres://user:password@host:5432/database
   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 
